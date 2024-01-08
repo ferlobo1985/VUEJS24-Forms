318bc186538b49fb33544b60542d10a42f2325f3
ferlobo1985 <fernando.lobo.prez@gmail.com>
2024-01-08 18:07:02 -0300

    04-Vee-Validate
    04-Vee-Validate

--- src/components/validate/index.vue
+++ src/components/validate/index.vue
@@ -1,15 +1,38 @@
 <template>
-
+    <Form>
         <div class="mb-3">
             <label for="name">Name</label>
+            <Field 
+              name="name" 
+              :rules=[isRequired,validateName]
+              placeholder="Enter your name"
+              class="form-control"  
+            />
+            <ErrorMessage name="name" as="div" v-slot="{message}">
+              <div class="alert alert-danger" role="alert">
+                {{ message }}
+              </div>
+            </ErrorMessage>
         </div>
 
         <button class="btn btn-primary">
           Submit
         </button>
-
+    </Form>
 </template>
 
 <script setup>
+  import { Field, Form, ErrorMessage } from 'vee-validate';
+
+  const isRequired = (value) =>{
+      if(!value) { return 'This field is required'}
+      return true;
+  }
+
+  const validateName = (value) => {
+    if(value !== 'steve'){ return true }
+    return 'You are not allowed steve !!!'
+  }
+
 
 </script>
